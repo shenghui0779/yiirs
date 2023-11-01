717b72ac1f5034c325fb68c73e15dbe97daa0a2e
shenghui <shenghui0779@gmail.com>
2023-11-01 12:01:17 +0800

    Add aes-gcm

--- src/util/aes.rs
+++ src/util/aes.rs
@@ -1,5 +1,5 @@
 use anyhow::{anyhow, Result};
-use openssl::symm::{Cipher, Crypter, Mode};
+use openssl::symm::{decrypt_aead, encrypt_aead, Cipher, Crypter, Mode};
 
 // AES-CBC pkcs#7
 // CBC(key, iv)
@@ -126,6 +126,43 @@ impl<'a> ECB<'a> {
     }
 }
 
+// AES-GCM
+// GCM(key, iv)
+#[allow(dead_code)]
+pub struct GCM<'a>(pub &'a [u8], pub &'a [u8]);
+
+#[allow(dead_code)]
+impl<'a> GCM<'a> {
+    fn cipher(&self) -> Result<Cipher> {
+        let cipher = match self.0.len() {
+            16 => Cipher::aes_128_gcm(),
+            24 => Cipher::aes_192_gcm(),
+            32 => Cipher::aes_256_gcm(),
+            _ => return Err(anyhow!("crypto/aes: invalid key size")),
+        };
+
+        Ok(cipher)
+    }
+
+    // tag_size: [12, 16]
+    pub fn encrypt(&self, data: &[u8], aad: &[u8], tag_size: usize) -> Result<(Vec<u8>, Vec<u8>)> {
+        let t = self.cipher()?;
+        let GCM(key, iv) = *self;
+        let mut tag = vec![0; tag_size];
+        let out = encrypt_aead(t, key, Some(iv), aad, data, &mut tag)?;
+
+        Ok((out, tag))
+    }
+
+    pub fn decrypt(&self, data: &[u8], aad: &[u8], tag: &[u8]) -> Result<Vec<u8>> {
+        let t = self.cipher()?;
+        let GCM(key, iv) = *self;
+        let out = decrypt_aead(t, key, Some(iv), aad, data, tag)?;
+
+        Ok(out)
+    }
+}
+
 fn pkcs7_padding(b: &[u8], block_size: usize) -> Vec<u8> {
     let mut padding = block_size - b.len() % block_size;
     if padding == 0 {
@@ -150,7 +187,7 @@ fn pkcs7_unpadding(b: &[u8]) -> Vec<u8> {
 mod tests {
     use base64::{prelude::BASE64_STANDARD, Engine};
 
-    use crate::util::aes::{CBC, ECB};
+    use crate::util::aes::{CBC, ECB, GCM};
 
     #[test]
     fn aes_cbc() {
@@ -192,5 +229,25 @@ mod tests {
 
         let plain2 = ecb.decrypt(&cipher2).unwrap();
         assert_eq!(plain2, b"ILoveYiigo");
+    }
+
+    #[test]
+    fn aes_gcm() {
+        let key = b"AES256Key-32Characters1234567890";
+        let gcm = GCM(key, &key[..12]);
+
+        let (cipher, tag) = gcm.encrypt(b"ILoveYiigo", b"IIInsomnia", 16).unwrap();
+        assert_eq!(BASE64_STANDARD.encode(&cipher), "qciumnRZKY42HQ==");
+        assert_eq!(BASE64_STANDARD.encode(&tag), "WOeD9xSN3RX44lkHpnBEXw==");
+
+        let plain = gcm.decrypt(&cipher, b"IIInsomnia", &tag).unwrap();
+        assert_eq!(plain, b"ILoveYiigo");
+
+        let (cipher2, tag2) = gcm.encrypt(b"ILoveYiigo", b"IIInsomnia", 12).unwrap();
+        assert_eq!(BASE64_STANDARD.encode(&cipher2), "qciumnRZKY42HQ==");
+        assert_eq!(BASE64_STANDARD.encode(&tag2), "WOeD9xSN3RX44lkH");
+
+        let plain = gcm.decrypt(&cipher2, b"IIInsomnia", &tag2).unwrap();
+        assert_eq!(plain, b"ILoveYiigo");
     }
 }
