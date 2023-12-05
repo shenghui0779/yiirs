9971aa55112b067b32c4929c620eead67b796659
shenghui <shenghui0779@gmail.com>
2023-12-05 15:09:24 +0800

    logger不再依赖配置文件

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,16 @@ async fn main() {
 
     // _guard 必须在 main 函数中才能使日志生效
     match matches.subcommand() {
+        // Command: serve
         Some(("serve", sub_matches)) => {
             let cfg = config::init(sub_matches.get_one::<String>("FILE").unwrap());
             let _guard = config::logger::init(Some(&cfg));
 
             cmd::server::serve(config::app_state(cfg).await).await;
         }
+        // Command: hello
         Some(("hello", _sub_matches)) => println!("hello world"),
+        // Unreachable
         _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
     }
 }
