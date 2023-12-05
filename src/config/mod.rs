55f62bf1de12e74454be75a21810cd4f82d2d9c3
shenghui <shenghui0779@gmail.com>
2023-12-05 11:58:00 +0800

    使用 clap 处理命令行

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -9,7 +9,8 @@ use tracing_appender::non_blocking::WorkerGuard;
 use crate::util::AppState;
 
 pub async fn init(cfg_file: &String) -> (AppState, WorkerGuard) {
-    let path = fs::canonicalize(cfg_file).unwrap_or_else(|e| panic!("配置文件加载失败：{}", e));
+    let path = fs::canonicalize(cfg_file)
+        .unwrap_or_else(|e| panic!("配置文件加载失败：{} - {}", e, cfg_file));
 
     let cfg = Config::builder()
         .add_source(config::File::with_name(path.to_str().unwrap()))
