a1262db35df240602ba1034e75aeb56f0e1dd3d8
shenghui <shenghui0779@gmail.com>
2023-12-05 15:05:50 +0800

    logger不再依赖配置文件

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -4,22 +4,22 @@ pub mod redis;
 
 use config::Config;
 use std::fs;
-use tracing_appender::non_blocking::WorkerGuard;
 
 use crate::util::AppState;
 
-pub async fn init(cfg_file: &String) -> (AppState, WorkerGuard) {
+pub fn init(cfg_file: &String) -> Config {
     let path = fs::canonicalize(cfg_file)
         .unwrap_or_else(|e| panic!("配置文件加载失败：{} - {}", e, cfg_file));
 
-    let cfg = Config::builder()
+    Config::builder()
         .add_source(config::File::with_name(path.to_str().unwrap()))
         .build()
-        .unwrap_or_else(|e| panic!("配置文件加载失败：{}", e));
+        .unwrap_or_else(|e| panic!("配置文件加载失败：{}", e))
+}
 
-    let _guard = logger::init(Some(&cfg));
+pub async fn app_state(cfg: Config) -> AppState {
     let db = db::init(&cfg).await;
     let redis = redis::init(&cfg);
 
-    (AppState { cfg, db, redis }, _guard)
+    AppState { cfg, db, redis }
 }
--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,10 @@ async fn main() {
     // _guard 必须在 main 函数中才能使日志生效
     match matches.subcommand() {
         Some(("serve", sub_matches)) => {
-            let (state, _guard) =
-                config::init(sub_matches.get_one::<String>("FILE").unwrap()).await;
-            cmd::server::serve(state).await;
+            let cfg = config::init(sub_matches.get_one::<String>("FILE").unwrap());
+            let _guard = config::logger::init(Some(&cfg));
+
+            cmd::server::serve(config::app_state(cfg).await).await;
         }
         Some(("hello", _sub_matches)) => println!("hello world"),
         _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
