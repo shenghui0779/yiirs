e7ec63520b01903dde62a30b101e49fec724f9b3
shenghui0779 <shenghui0779@gmail.com>
2024-11-01 14:25:50 +0800

    Update core

--- src/shared/core/db.rs
+++ src/shared/core/db.rs
@@ -19,27 +19,28 @@ pub fn conn() -> &'static DatabaseConnection {
 pub async fn new(cfg: &Config, key: &str) -> anyhow::Result<DatabaseConnection> {
     let mut opt = ConnectOptions::new(cfg.get_string(&format!("{}.dsn", key))?);
 
-    opt.min_connections(
-        cfg.get_int(&format!("{}.options.min_conns", key))
-            .unwrap_or(10) as u32,
-    )
-    .max_connections(
-        cfg.get_int(&format!("{}.options.max_conns", key))
-            .unwrap_or(20) as u32,
-    )
-    .connect_timeout(Duration::from_secs(
-        cfg.get_int(&format!("{}.options.conn_timeout", key))
-            .unwrap_or(10) as u64,
-    ))
-    .idle_timeout(Duration::from_secs(
-        cfg.get_int(&format!("{}.options.idle_timeout", key))
-            .unwrap_or(300) as u64,
-    ))
-    .max_lifetime(Duration::from_secs(
-        cfg.get_int(&format!("{}.options.max_lifetime", key))
-            .unwrap_or(600) as u64,
-    ))
-    .sqlx_logging(cfg.get_bool("app.debug").unwrap_or_default());
+    let min_conns = cfg
+        .get_int(&format!("{}.options.min_conns", key))
+        .unwrap_or(10);
+    let max_conns = cfg
+        .get_int(&format!("{}.options.max_conns", key))
+        .unwrap_or(20);
+    let conn_timeout = cfg
+        .get_int(&format!("{}.options.conn_timeout", key))
+        .unwrap_or(10);
+    let idle_timeout = cfg
+        .get_int(&format!("{}.options.idle_timeout", key))
+        .unwrap_or(300);
+    let max_lifetime = cfg
+        .get_int(&format!("{}.options.max_lifetime", key))
+        .unwrap_or(600);
+
+    opt.min_connections(min_conns as u32)
+        .max_connections(max_conns as u32)
+        .connect_timeout(Duration::from_secs(conn_timeout as u64))
+        .idle_timeout(Duration::from_secs(idle_timeout as u64))
+        .max_lifetime(Duration::from_secs(max_lifetime as u64))
+        .sqlx_logging(cfg.get_bool("app.debug").unwrap_or_default());
 
     let conn = Database::connect(opt).await?;
     let _ = conn.ping().await?;
